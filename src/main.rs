744e9fd293297e12db0f422512b4cd83886604e0
Anirudh Konidala <kidskodingclub@gmail.com>
2025-03-11 10:24:39 -0500

    added a running web server

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+extern crate tokio;
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new()
+        .route("/", get(|| async { "practicestax!" }));
+    let localhost_url = "127.0.0.1:3000";
+
+    let listener = tokio::net::TcpListener::bind(localhost_url)
+        .await?;
+
+    println!("Server running on {localhost_url}");
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
